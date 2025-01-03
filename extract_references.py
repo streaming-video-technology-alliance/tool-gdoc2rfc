8d564e1cc3cd85590c86d38b24476062da439bef
glenng <58149955+glenng1215gh@users.noreply.github.com>
2025-01-03 15:55:16 -0500

    Added [SVTAxxxx] reference conversion to IETF drafts.
    added mapping of SVTAxxx documents to IETF draft references. cleaned up code.

--- extract_references.py
+++ extract_references.py
@@ -7,7 +7,60 @@ import os
 import re
 import configparser
 
+# Dictionary of referenced SVTA docs that have IETF drafts
+svta_drafts = {
+  "[SVTA2031]": "power-metadata-expression-language",
+  "[SVTA2032]": "goldstein-processing-stages-metadata",
+  "[SVTA2033]": "ietf-cdni-cache-control-metadata",
+  "[SVTA2034]": "chaudhari-source-access-control-metadata",
+  "[SVTA2035]": "ietf-cdni-client-access-control-metadata",
+  "[SVTA2036]": "ietf-cdni-edge-control-metadata",
+  "[SVTA2037]": "bichot-delivery-metadata",
+  "[SVTA2038]": "warshavsky-private-features-metadata",
+  "[SVTA2039]": "ietf-cdni-protected-secrets-metadata"
+}
 
+# List of external documents to link to the reference section
+external_docs = {
+  "[CDNI-MEL]",
+  "[AWSv4]",
+  "[X.509]",
+  "[HCVAULT]",
+  "[URI.signing]",
+  "[W3C]",
+  "[PCRE]",
+  "[WHATWG-FETCH]",
+  "[CTA-5007]"
+  }
+
+
+def replace_svta_draft_references(filedata):
+  for key, value in svta_drafts.items():
+    if (key in filedata):
+      print (f'\nReplacing {key} with IETF draft reference {value}')
+      print (f'Be sure to add reference in rfc_format.xml for <xi:include href="https://bib.ietf.org/public/rfc/bibxml3/reference.I-D.{value}.xml"/>')
+     
+      replacement = f'<xref target="I-D.{value}"/>'
+      filedata = filedata.replace(key, replacement)
+  
+  return filedata
+
+def replace_external_doc_references(filedata):
+  for value in external_docs:
+    if (value in filedata):
+      start = value.index('[') + 1
+      end = value.index(']')
+      anchor = value[start:end]
+      print (f'\nReplacing {value} with link to external reference.')
+      print (f'Be sure to add reference in rfc_format.xml with anchor="{anchor}"')
+
+      replacement = f'<xref target="{anchor}"/>'
+      filedata = filedata.replace(value, replacement)
+  
+  return filedata
+
+
+# Main program
 config_parser = configparser.ConfigParser()
 config_parser.read('configuration.conf')
 config = config_parser['extract_docx']
@@ -18,6 +71,7 @@ csvfiles = [] #list to store all csv files found at location
 filebeginwithhello = [] # list to keep all files that begin with 'hello'
 otherfiles = [] #list to keep any other file that do not match the criteria
 
+
 for filename in os.listdir(location):
     # Read in the file
     print ('\nExtracting references for:', filename)
@@ -25,29 +79,31 @@ for filename in os.listdir(location):
       filedata = file.read()
 
     # Replace all [RFCxxx] references
-    print ("RFC document references:", re.findall('\[RFC(\d{4})\]', filedata))
-    pattern = r'\[RFC(\d{4})\]'
-    filedata = re.sub(pattern, r'<xref target="RFC\1" />', filedata)
+    refs = re.findall('\[RFC(\d{4})\]', filedata)
+    if (refs):
+      print ("RFC document references:", refs)
+      pattern = r'\[RFC(\d{4})\]'
+      filedata = re.sub(pattern, r'<xref target="RFC\1" />', filedata)
  
-    # Replace all [SVTAxxx] references
-    print ("SVTA document references:", re.findall('\[SVTA(\d{4})\]', filedata))
-    pattern = r'\[SVTA(\d{4})\]'
-    filedata = re.sub(pattern, r'<xref target="SVTA\1" />', filedata)
+    # Replace all [ietf*] references
+    refs = re.findall('\[ietf([^\]]*)\]', filedata)
+    if (refs):
+      print ("IETF draft document references:", refs)
+      pattern = r'\[ietf([^\]]*)\]'
+      filedata = re.sub(pattern, r'<xref target="ietf\1" />', filedata)
 
-   # Replace all [ietf*] references
-    print ("IETF draft document references:", re.findall('\[ietf([^\]]*)\]', filedata))
-    pattern = r'\[ietf([^\]]*)\]'
-    filedata = re.sub(pattern, r'<xref target="ietf\1" />', filedata)
+    # Replace [SVTAxxxx] references with internet draft references for SVTA docs that have IETF drafts
+    refs = re.findall('\[SVTA(\d{4})\]', filedata)
+    if (refs):
+      print ("SVTA document references:", refs)
+      filedata = replace_svta_draft_references(filedata)
 
-    # Replace other misc references  
-    filedata = filedata.replace('[CDNI-MEL]', '<xref target="CDNI-MEL" />')
-    filedata = filedata.replace('[AWSv4]', '<xref target="AWSv4" />')
-    filedata = filedata.replace('[X.509]', '<xref target="X.509" />')
-    filedata = filedata.replace('[HCVAULT]', '<xref target="HCVAULT" />')
-    filedata = filedata.replace('[URI.signing]', '<xref target="URI.signing" />')
-    filedata = filedata.replace('[W3C]', '<xref target="W3C" />')
-    filedata = filedata.replace('[PCRE]', '<xref target="PCRE" />')
+      # Replace all remaining [SVTAxxx] references with an XREF
+      pattern = r'\[SVTA(\d{4})\]'
+      filedata = re.sub(pattern, r'<xref target="SVTA\1" />', filedata)
 
+    # Replace other misc references
+    filedata =  replace_external_doc_references(filedata)
 
     # Write the file out again
     with open(location+filename, 'w') as file:
